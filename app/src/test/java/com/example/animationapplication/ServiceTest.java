0bc5ba954a246353d7ad73ea3e9069a6d38528f8
kouichiUme <kouichiume@gmail.com>
2021-04-19 23:43:11 +0900

    commit first android application test and log in test case

---
+++ app/src/test/java/com/example/animationapplication/ServiceTest.java
@@ -0,0 +1,27 @@
+package com.example.animationapplication;
+
+import android.util.Log;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ServiceTest {
+
+    @Test
+    public void test_AssertEquation() {
+    //
+    //        Log.d("Debug ","first test class");
+
+        assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void test_Logging(){
+
+            System.out.print("systemout ");
+
+    }
+}
+
